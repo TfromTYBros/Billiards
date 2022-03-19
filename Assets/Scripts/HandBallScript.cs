decb28cc5c4a3c83d17dabfb9adc9817411bc337
TfromTYBros <kanshashitemasu358@gmail.com>
2022-03-20 04:32:48 +0900

    どの角度からも打てる様にHandBallに実装。 次はフィールドのcolliderかrigid 2dを付ける。

--- Assets/Scripts/HandBallScript.cs
+++ Assets/Scripts/HandBallScript.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class HandBallScript : MonoBehaviour
 {
     new Rigidbody2D rigidbody2D;
-    Vector2 DebugForce = new Vector2(10.0f, 0.0f);
+    //Vector2 DebugForce = new Vector2(10.0f, 0.0f);
     bool stop = false;
+    float speed = 0.0f;
 
     void Start()
     {
         rigidbody2D = this.GetComponent<Rigidbody2D>();
+        SetSpeed(10.0f);
     }
 
     void Update()
@@ -20,18 +22,48 @@ public class HandBallScript : MonoBehaviour
             Debug.Log("SpaceKey->Debug");
             DebugAddForce();
             stop = true;
-            Debug.Log(ZtoX());
+            Debug.Log("ZtoX(): " + ZtoX());
+            Debug.Log("ZtoY(): " + ZtoY());
         }
     }
 
     void DebugAddForce()
     {
         //rigidbody2D.AddForce(DebugForce, ForceMode2D.Impulse);
-        rigidbody2D.AddForce(this.transform.forward, ForceMode2D.Impulse);
+        rigidbody2D.AddForce(new Vector3(ZtoX(), ZtoY(), 0.0f), ForceMode2D.Impulse);
+    }
+
+    public void SetSpeed(float speedValue)
+    {
+        speed = speedValue;
     }
 
     private float ZtoX()
     {
-        return (this.gameObject.transform.localEulerAngles.z % 90);
+        float ballz = this.gameObject.transform.localEulerAngles.z % 360.0f;
+        if (90 <= ballz && ballz <= 270)
+        {
+            return speed * (Mathf.Abs((ballz % 180) - 90) / 90) * -1;
+        }
+        else
+        {
+            return speed * (Mathf.Abs((ballz % 180) - 90) / 90);
+        }
+    }
+
+    private float ZtoY()
+    {
+        float ballz = this.gameObject.transform.localEulerAngles.z % 360.0f;
+        //Debug.Log("ballz: " + ballz);
+        if(0 <= ballz && ballz <= 180)
+        {
+            //Debug.Log("0 <= 180");
+            return speed - (speed * (Mathf.Abs((ballz % 180) - 90) / 90));
+        }
+        else
+        {
+            //Debug.Log("180 <= 360");
+            return (speed - (speed * (Mathf.Abs((ballz % 180) - 90) / 90))) * -1;
+        }
     }
 }
